f7e4095b7ad9a07f000ab0e31cfcc6fd1a49941b
conrongvang <long.nguyenhoang7996@gmail.com>
2025-02-18 11:34:20 +0700

    config: env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 
 	config "booking-apartment/configs"
 	"booking-apartment/internal/routers"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("❌ Error loading .env file: %v", err)
+	}
+
+	port := os.Getenv("GATEWAY_PORT")
+
 	config.ConnectDatabase()
 
 	router := gin.Default()
 	routers.SetupRoutes(router)
 
-	router.Run(":3333")
+	router.Run(":" + port)
 }
